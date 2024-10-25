25599feac63d2378180d27b24ad2d667adc8658a
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-10-25 10:34:57 +0700

    refactor: implement header to menu page

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,14 +1,10 @@
 import GoTo from "@/components/ui/goTo";
-import Image from "next/image";
-import Setting from "@/components/setting";
+import Header from "@/components/header";
 
 export default function Page() {
     return (
         <div className="bg-main-menu min-h-screen bg-cover flex flex-col items-center justify-center">
-            <div className="flex w-full fixed top-0 justify-between py-5 px-5">
-                <Image src="/logo-sm-white.svg" alt="logo" width={200} height={200}/>
-                <Setting/>
-            </div>
+            <Header backTo="/" />
             <div className="flex gap-4">
                 <GoTo href="/menu/flora" icon="mdi:flower" text="Flora"/>
                 <GoTo href="/menu/fauna" icon="dashicons:pets" text="Fauna"/>
