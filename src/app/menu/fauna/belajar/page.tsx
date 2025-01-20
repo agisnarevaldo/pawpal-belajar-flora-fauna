dd5ec98918ded886cdc82e440693cb2a0cb2e5ca
Agisna Revaldo <agisrifaldo75@gmail.com>
2025-01-21 04:16:52 +0700

    refactor: add additional images for animals and flowers

--- src/app/menu/fauna/belajar/page.tsx
+++ src/app/menu/fauna/belajar/page.tsx
@@ -14,31 +14,37 @@ const animals = [
     {
         name: "B-A-D-A-K",
         image: "/animals/badak.svg",
+        image2: "/animals/badak2.svg",
         description: "Badak adalah hewan besar dengan kulit tebal dan keras, serta memiliki satu atau dua tanduk di hidungnya. Mereka hidup di padang rumput dan hutan, dan suka makan rumput serta dedaunan. Mereka biasanya hidup sendiri atau dalam kelompok kecil, Tanduk badak digunakan untuk melindungi diri dari pemangsa."
     },
     {
         name: "H-A-R-I-M-A-U",
         image: "/animals/harimau.svg",
+        image2: "/animals/harimau2.svg",
         description: "Harimau adalah hewan besar dan kuat yang hidup di hutan. Bulunya berwarna oranye dengan garis-garis hitam, yang membantu mereka bersembunyi saat berburu. Harimau suka makan daging. Mereka adalah perenang yang hebat dan suka berada di dekat air."
     },
     {
         name: "J-E-R-A-P-A-H",
         image: "/animals/jerapah.svg",
+        image2: "/animals/jerapah2.svg",
         description: "Jerapah adalah hewan tertinggi di dunia, dengan leher yang sangat panjang. Leher panjang ini membantu mereka makan daun-daun tinggi di pohon yang tidak bisa dijangkau hewan lain. Jerapah memiliki pola bintik-bintik cokelat di tubuhnya yang unik untuk setiap jerapah. Jerapah hidup di padang rumput dan hutan, serta suka hidup dalam kelompok yang disebut kawanan."
     },
     {
         name: "M-O-N-Y-E-T",
         image: "/animals/monyet.svg",
+        image2: "/animals/monyet2.svg",
         description: "Monyet adalah hewan yang suka bermain dan melompat-lompat di pohon. Mereka punya ekor panjang yang membantu menjaga keseimbangan saat memanjat. Monyet suka makan buah-buahan seperti pisang, apel, dan mangga. Mereka hidup di hutan. Monyet juga pintar dan bisa meniru suara atau gerakan yang mereka lihat."
     },
     {
         name: "S-I-N-G-A",
         image: "/animals/singa.svg",
+        image2: "/animals/singa2.svg",
         description: "Singa adalah hewan besar dan kuat yang dikenal sebagai \"raja hutan.\" Mereka hidup di padang rumput dan hutan. Singa jantan memiliki surai tebal di sekitar kepalanya, sedangkan singa betina tidak. Singa suka berburu bersama dalam kelompok yang disebut kawanan. Mereka biasanya berburu hewan seperti zebra dan rusa."
     },
     {
         name: "Z-E-B-R-A",
         image: "/animals/zebra.svg",
+        image2: "/animals/zebra2.svg",
         description: "Zebra adalah hewan yang memiliki corak garis-garis hitam dan putih di tubuhnya. Zebra hidup di padang rumput dan sering bergerombol dengan kawanannya. Mereka makan rumput dan daun-daunan. Zebra adalah pelari cepat, sehingga mereka bisa melarikan diri dari hewan pemangsa seperti singa."
     }
 ]
--- src/app/menu/flora/belajar/page.tsx
+++ src/app/menu/flora/belajar/page.tsx
@@ -14,31 +14,37 @@ const flowers = [
     {
         name: "B-U-N-G-A   M-A-T-A-H-A-R-I",
         image: "/flowers/matahari.svg",
+        image2: "/flowers/matahari2.svg",
         description: "Bunga matahari adalah bunga besar yang berwarna kuning cerah dan selalu menghadap ke arah matahari. Bunga ini tumbuh tinggi dan bijinya bisa dimakan. Anak-anak suka karena bentuknya mirip matahari di langit",
     },
     {
         name: 'B-U-N-G-A   L-I-L-I',
         image: "/flowers/lili.svg",
+        image2: "/flowers/lili2.svg",
         description: "Bunga lili adalah bunga yang cantik dengan kelopak lembut yang sering berwarna putih, merah muda, atau kuning. Bunga ini tumbuh di batang yang panjang dan memiliki aroma harum yang menyenangkan. Bunga lili sering digunakan untuk hiasan karena keindahannya.",
     },
     {
         name: "B-U-N-G-A   M-A-W-A-R",
         image: "/flowers/mawar.svg",
+        image2: "/flowers/mawar2.svg",
         description: "Bunga mawar adalah bunga yang indah dengan kelopak yang berlapis-lapis dan sering berwarna merah, putih, atau merah muda. Bunga ini terkenal karena keharumannya dan duri kecil di batangnya. Mawar sering diberikan sebagai simbol cinta dan persahabatan.",
     },
     {
         name: "B-U-N-G-A   D-A-I-S-Y",
         image: "/flowers/daisy.svg",
+        image2: "/flowers/daisy2.svg",
         description: "Bunga daisy adalah bunga kecil yang lucu dengan kelopak berwarna kuning cerah dan bagian tengahnya berwarna coklat tua. Bentuknya sederhana dan sering ditemukan di taman atau padang rumput. Daisy melambangkan keceriaan dan kesederhanaan, sehingga banyak disukai anak-anak. ",
     },
     {
         name: "B-U-N-G-A   R-E-U-L-L-I-A",
         image: "/flowers/reulia.svg",
+        image2: "/flowers/reulia2.svg",
         description: "Bunga ruellia adalah bunga berwarna cerah yang biasanya berwarna ungu, merah muda, atau biru. Bunga ini tumbuh di semak-semak kecil dan sering mekar sepanjang tahun. Ruellia mudah ditemukan di taman dan sering tumbuh liar, membuat lingkungan tampak lebih indah dengan warnanya yang menarik.",
     },
     {
         name: "B-U-N-G-A   A-N-G-G-R-E-K",
         image: "/flowers/anggrek.svg",
+        image2: "/flowers/anggrek2.svg",
         description: "Bunga anggrek adalah bunga yang cantik dan eksotis dengan berbagai warna, seperti putih, biru, kuning, dan merah muda. Bunga ini tumbuh di batang yang panjang dan sering terlihat anggun serta elegan. Anggrek biasanya tumbuh di tempat yang hangat dan lembap, dan dikenal karena bentuknya yang unik dan indah."
     }
 ]
